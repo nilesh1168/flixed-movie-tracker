aa91e3bc3da3a3c85167e6d8b8b3d53b9ecdc060
nilesh1168 <n.suryawanshi1168@gmail.com>
2021-02-17 23:11:35 +0530

    added drf-jwt support

--- flixed_django/flixedREST/serializers.py
+++ flixed_django/flixedREST/serializers.py
@@ -1,5 +1,44 @@
 from .models import WatchedMovies, WatchListMovies
 from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+        Used to load data when user is already logged in
+    """
+    class Meta:
+        model = User
+        fields = ('username',)
+
+class UserSerializerWithToken(serializers.ModelSerializer):
+    """
+        Used to store and return data when user signs up
+    """
+    token = serializers.SerializerMethodField()
+    password = serializers.CharField(write_only=True)
+
+    def get_token(self, obj):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+        return token
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    class Meta:
+        model = User
+        fields = ('token', 'username', 'password')
+
 
 class WatchedMoviesSerializer(serializers.ModelSerializer):
     class Meta:
--- flixed_django/flixed_django/settings.py
+++ flixed_django/flixed_django/settings.py
@@ -59,6 +59,23 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+JWT_AUTH = {
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'flixed_django.utils.my_jwt_response_handler'
+}
+
+
+
 ROOT_URLCONF = 'flixed_django.urls'
 
 TEMPLATES = [
