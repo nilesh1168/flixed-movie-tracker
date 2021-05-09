d38ca645d1eda3f67340249114a7309af4d0b428
nilesh1168 <n.suryawanshi1168@gmail.com>
2021-05-09 16:07:42 +0530

    added more fields for new user

--- flixed_reactjs/src/components/RegisterForm.js
+++ flixed_reactjs/src/components/RegisterForm.js
@@ -1,11 +1,15 @@
-import { Container, Row, Col, Card, Form, Button } from "react-bootstrap"
+import { Container, Row, Col, Card, Form, Button, FormGroup, FormLabel } from "react-bootstrap"
 import React from 'react';
 
-class RegisterForm extends React.Component{
+class RegisterForm extends React.Component {
     state = {
         username: '',
+        email: '',
+        first_name: '',
+        last_name: '',
         password: '',
-        confirm_password:''
+        confirm_password: '',
+        error: ''
     };
 
     handle_change = e => {
@@ -17,35 +21,85 @@ class RegisterForm extends React.Component{
             return newState;
         });
     };
-    render(){
+
+    validate(e) {
+        this.setState({
+            error: ""
+        })
+        if (this.state.password === this.state.confirm_password) {
+            var data = {
+                "email": this.state.email,
+                "first_name": this.state.first_name,
+                "last_name": this.state.last_name,
+                "username": this.state.username,
+                "password": this.state.password,
+
+            }
+            console.log(e, data)
+            this.props.handle_signup(e, data)
+        }
+        else {
+            this.setState({
+                error: "Password's do not match!!"
+            })
+        }
+    }
+
+    render() {
         return (
             <Container>
                 <Row>
-                    <Col className="my-5 ml-auto col-md-4">
+                    <Col className="my-5 ml-auto col-md-8">
                         <Card>
                             <Card.Body>
                                 <Card.Title className='text-center my-3'>Sign Up</Card.Title>
                                 <Card.Text>
                                     <Form>
-                                        <Form.Group controlId="formBasicEmail">
-                                            <Form.Label>Username</Form.Label>
-                                            <Form.Control name="username" value={this.state.username}
-                                                onChange={this.handle_change} type="text" placeholder="Username" />
-                                        </Form.Group>
-    
-                                        <Form.Group controlId="formBasicPassword">
-                                            <Form.Label>Password</Form.Label>
-                                            <Form.Control name="password" value={this.state.password}
-                                                onChange={this.handle_change} type="password" placeholder="Password" />
-                                        </Form.Group>
-    
-                                        <Form.Group controlId="formBasicPassword">
-                                            <Form.Label>Confirm Password</Form.Label>
-                                            <Form.Control name="confirm_password" value={this.state.confirm_password}
-                                                onChange={this.handle_change} type="password" placeholder="Confirm Password" />
-                                        </Form.Group>
-    
-                                        <Button className='my-4 mx-auto d-block' variant="primary" type="submit">Submit</Button>
+                                        <Row>
+                                            <Col className="col-md-6">
+                                                <Form.Group controlId="formBasicUsername">
+                                                    <Form.Label>Username</Form.Label>
+                                                    <Form.Control name="username" value={this.state.username}
+                                                        onChange={this.handle_change} type="text" placeholder="Username" />
+                                                </Form.Group>
+
+                                                <Form.Group controlId="formBasicFName">
+                                                    <Form.Label>First Name</Form.Label>
+                                                    <Form.Control name="first_name" value={this.state.first_name}
+                                                        onChange={this.handle_change} type="text" placeholder="First Name" />
+                                                </Form.Group>
+                                                <Form.Group controlId="formBasicPassword">
+                                                    <Form.Label>Password</Form.Label>
+                                                    <Form.Control name="password" value={this.state.password}
+                                                        onChange={this.handle_change} type="password" placeholder="Password" />
+                                                </Form.Group>
+                                            </Col>
+                                            <Col className="col-md-6">
+                                                <Form.Group controlId="formBasicEmail">
+                                                    <Form.Label>Email</Form.Label>
+                                                    <Form.Control name="email" value={this.state.email}
+                                                        onChange={this.handle_change} type="text" placeholder="Email" />
+                                                </Form.Group>
+
+                                                <Form.Group controlId="formBasicFName">
+                                                    <Form.Label>Last Name</Form.Label>
+                                                    <Form.Control name="last_name" value={this.state.last_name}
+                                                        onChange={this.handle_change} type="text" placeholder="Last Name" />
+                                                </Form.Group>
+
+                                                <Form.Group controlId="formBasicPassword">
+                                                    <Form.Label>Confirm Password</Form.Label>
+                                                    <Form.Control name="confirm_password" value={this.state.confirm_password}
+                                                        onChange={this.handle_change} type="password" placeholder="Confirm Password" />
+                                                </Form.Group>
+                                            </Col>
+                                        </Row>
+                                        
+                                        <FormGroup className="text-center">
+                                            <FormLabel><p style={{ color: "red" }}>{this.state.error}</p></FormLabel>
+                                        </FormGroup>
+
+                                        <Button className='my-4 mx-auto d-block' variant="primary" onClick={e => this.validate(e)}>Submit</Button>
                                     </Form>
                                 </Card.Text>
                             </Card.Body>
@@ -55,7 +109,7 @@ class RegisterForm extends React.Component{
             </Container>
         )
     }
-    
+
 }
 
 export default RegisterForm;
