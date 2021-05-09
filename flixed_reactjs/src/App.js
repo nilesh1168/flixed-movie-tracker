51f2c2500a20f097b302da0809e55b513f8a17f3
nilesh1168 <n.suryawanshi1168@gmail.com>
2021-05-09 11:30:28 +0530

    changed func to class component

--- flixed_reactjs/src/App.js
+++ flixed_reactjs/src/App.js
@@ -1,44 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './styles/App.module.css'
 import React from 'react'
-import Container from "react-bootstrap/Container";
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row'
-import SearchMovies from './components/SearchMovies'
-import WatchList from './components/WatchList'
-import WatchedMovies from './components/WatchedMovies'
 import LoginForm from './components/LoginForm'
 import RegisterForm from './components/RegisterForm'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Home from './components/Home'
-import { isEqual } from 'lodash';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect,
-  useHistory,
-  useLocation
 } from "react-router-dom";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      watchedList: [],
-      watchList: [],
-      searchedMovie: {},
-      error: "",
       logged_in: localStorage.getItem('token') ? true : false,
       user: ''
     }
-    this.handleWatchListAdd = this.handleWatchListAdd.bind(this);
-    this.handleWatchedListAdd = this.handleWatchedListAdd.bind(this);
-    this.handleWatchListDelete = this.handleWatchListDelete.bind(this);
-    this.handleSearchedMovie = this.handleSearchedMovie.bind(this);
-    this.handleError = this.handleError.bind(this);
     this.handle_login = this.handle_login.bind(this)
     this.handle_logout = this.handle_logout.bind(this)
     this.handle_signup = this.handle_signup.bind(this)
@@ -60,91 +41,6 @@ class App extends React.Component {
     else {
       console.log('User needs to log in')
     }
-
-
-    // const watchedRequest = new Request('http://127.0.0.1:8000/movies/watched', { method: 'GET' })
-    // const watchListRequest = new Request('http://127.0.0.1:8000/movies/watch_list', { method: 'GET' })
-    // fetch(watchedRequest)
-    //   .then(response => {
-    //     if (response.ok)
-    //       return response.json()
-    //     else
-    //       throw new Error("Something went wrong!!!")
-    //   })
-    //   .then(data => {
-    //     var movies = []
-    //     data.map(movie => (
-    //       movies.push({ "title": movie.title, "id": movie.id })
-    //     ))
-    //     this.setState({
-    //       watchedList: movies
-    //     })
-    //   })
-    //   .catch(error => {
-    //     this.setState({
-    //       error: error.message
-    //     })
-    //   })
-    // fetch(watchListRequest)
-    //   .then(response => {
-    //     if (response.ok)
-    //       return response.json()
-    //     else
-    //       throw new Error("Something went wrong!!!")
-    //   })
-    //   .then(data => {
-    //     var movies = []
-    //     data.map(movie => (
-    //       movies.push({ "title": movie.title, "id": movie.id })
-    //     ))
-    //     this.setState({
-    //       watchList: movies
-    //     })
-    //   })
-    //   .catch(error => {
-    //     this.setState({
-    //       error: error.message
-    //     })
-    //   })
-  }
-
-  handleError(message) {
-    this.setState({
-      error: message
-    })
-  }
-
-  handleSearchedMovie(movie) {
-    this.setState({
-      searchedMovie: movie
-    })
-  }
-
-  handleWatchListAdd(watchMovie) {
-    this.setState(
-      {
-        watchList: [...this.state.watchList, watchMovie]
-      }
-    )
-  }
-
-  handleWatchListDelete(watchMovieIds) {
-    watchMovieIds.forEach(delmovie => {
-      this.state.watchList.map((movie, index) => (
-        isEqual(JSON.stringify(delmovie), JSON.stringify(movie)) ? this.state.watchList.splice(index, 1) : console.log("false")
-      ))
-    });
-    this.setState({
-      watchList: this.state.watchList
-    })
-  }
-
-  handleWatchedListAdd(watchedMovie) {
-    this.setState(
-      {
-        watchedList: this.state.watchedList.concat(watchedMovie)
-      }
-    )
   }
 
   handle_login = (e, data) => {
@@ -163,7 +59,7 @@ class App extends React.Component {
           logged_in: true,
           user: json.user.username
         });
-
+        console.log("login success!!")
       });
   };
 
@@ -191,45 +87,16 @@ class App extends React.Component {
     this.setState({ logged_in: false, user: '' });
   };
 
-  // render() {
-  //   var page;
-  //   this.state.logged_in ? 
-  //   page =
-  //     <Container>
-  //       <NavBar user={this.state.user} logged_in={this.state.logged_in} handle_logout={this.handle_logout}/>
-  //       <Container>
-  //         <Row>
-  //           <Container>
-  //             <Row></Row>
-  //           </Container>
-  //         </Row>
-  //         <SearchMovies error={this.state.error} handleError={this.handleError} searchedMovie={this.state.searchedMovie} handleSearchedMovie={this.handleSearchedMovie} handleWatchListAdd={this.handleWatchListAdd} handleWatchedListAdd={this.handleWatchedListAdd} />
-  //         <Row>
-  //           <Col>
-  //             <WatchedMovies watchedList={this.state.watchedList} />
-  //           </Col>
-  //           <Col>
-  //             <WatchList handleWatchedListAdd={this.handleWatchedListAdd} handleWatchListDelete={this.handleWatchListDelete} watchList={this.state.watchList} />
-  //           </Col>
-  //         </Row>
-
-  //       </Container>
-  //     </Container>
-
-  //   :
-  //   page = <LandingPage user={this.state.user} handle_login={this.handle_login} logged_in={this.state.logged_in} handle_signup = { this.handle_signup}/>
-  //   return(page)
-  // }
-
   render() {
-    return(
+    console.log("app.js render")
+    return (
       <Router>
         <div className={styles.App}>
           <NavBar user={this.state.user} handle_login={this.state.handle_login} logged_in={this.state.logged_in} handle_logout={this.handle_logout} />
           <div className={styles.main}>
             <Switch>
               <Route path='/home'>
-                <Home logged_in={this.state.logged_in}/>
+                <Home logged_in={this.state.logged_in} />
               </Route>
               <Route path="/login">
                 <LoginForm handle_login={this.handle_login} />
@@ -240,6 +107,10 @@ class App extends React.Component {
               <Route path="/">
               </Route>
             </Switch>
+              {
+                this.state.logged_in ? <Redirect to="/home"/>  : 
+                <Redirect to="/login"/>
+              }
           </div>
           <Footer />
         </div>
--- flixed_reactjs/src/components/Home.js
+++ flixed_reactjs/src/components/Home.js
@@ -1,12 +1,158 @@
-import { Route, Redirect} from "react-router-dom";
-function Home(props){
-    if(props.logged_in){
+import { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { isEqual } from "lodash";
+import SearchMovies from "./SearchMovies"
+import WatchedMovies from "./WatchedMovies"
+import WatchList from "./WatchList"
 
-        console.log("Loggign")
+
+class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            watchedList: [],
+            watchList: [],
+            searchedMovie: {},
+            error: "",
+        }
+        this.handleWatchListAdd = this.handleWatchListAdd.bind(this);
+        this.handleWatchedListAdd = this.handleWatchedListAdd.bind(this);
+        this.handleWatchListDelete = this.handleWatchListDelete.bind(this);
+        this.handleSearchedMovie = this.handleSearchedMovie.bind(this);
+        this.handleError = this.handleError.bind(this);
+    }
+
+    componentDidMount() {
+        const watchedRequest = new Request('http://127.0.0.1:8000/movies/watched', { 
+            method: 'GET',
+            headers: { Authorization: `JWT ${localStorage.getItem('token')}`}
+         })
+        const watchListRequest = new Request('http://127.0.0.1:8000/movies/watch_list', { 
+            method: 'GET',
+            headers: { Authorization: `JWT ${localStorage.getItem('token')}`}
+        })
+        fetch(watchedRequest)
+            .then(response => {
+                if (response.ok)
+                    return response.json()
+                else
+                    throw new Error("Something went wrong!!!")
+            })
+            .then(data => {
+                var movies = []
+                data.map(movie => (
+                    movies.push({ "title": movie.title, "id": movie.id })
+                ))
+                this.setState({
+                    watchedList: movies
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    error: error.message
+                })
+            })
+        fetch(watchListRequest)
+            .then(response => {
+                if (response.ok)
+                    return response.json()
+                else
+                    throw new Error("Something went wrong!!!")
+            })
+            .then(data => {
+                var movies = []
+                data.map(movie => (
+                    movies.push({ "title": movie.title, "id": movie.id })
+                ))
+                this.setState({
+                    watchList: movies
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    error: error.message
+                })
+            })
     }
 
 
-    return <Redirect to="/login"/>
+    handleError(message) {
+        this.setState({
+            error: message
+        })
+    }
+
+    handleSearchedMovie(movie) {
+        this.setState({
+            searchedMovie: movie
+        })
+    }
+
+    handleWatchListAdd(watchMovie) {
+        this.setState(
+            {
+                watchList: [...this.state.watchList, watchMovie]
+            }
+        )
+    }
+
+    handleWatchListDelete(watchMovieIds) {
+        watchMovieIds.forEach(delmovie => {
+            this.state.watchList.map((movie, index) => (
+                isEqual(JSON.stringify(delmovie), JSON.stringify(movie)) ? this.state.watchList.splice(index, 1) : console.log("false")
+            ))
+        });
+        this.setState({
+            watchList: this.state.watchList
+        })
+    }
+
+    handleWatchedListAdd(watchedMovie) {
+        this.setState(
+            {
+                watchedList: this.state.watchedList.concat(watchedMovie)
+            }
+        )
+
+    }
+
+    render() {
+        console.log("inside home " + this.props.logged_in)
+        if (this.props.logged_in) {
+            console.log("logged in Home!")
+            return (
+                <Container>
+                    <Row>
+                        <Container>
+                            <Row></Row>
+                        </Container>
+                    </Row>
+                    <SearchMovies error={this.state.error}
+                        handleError={this.handleError}
+                        searchedMovie={this.state.searchedMovie}
+                        handleSearchedMovie={this.handleSearchedMovie}
+                        handleWatchListAdd={this.handleWatchListAdd}
+                        handleWatchedListAdd={this.handleWatchedListAdd}
+                    />
+                    <Row className="mb-4">
+                        <Col>
+                            <WatchedMovies watchedList={this.state.watchedList} />
+                        </Col>
+                        <Col>
+                            <WatchList handleWatchedListAdd={this.handleWatchedListAdd}
+                                handleWatchListDelete={this.handleWatchListDelete}
+                                watchList={this.state.watchList} />
+                        </Col>
+                    </Row>
+                </Container>
+            )
+        }
+        else {
+            console.log("not logged home!!")
+            return null
+        }
+    }
+
 }
 
 export default Home
