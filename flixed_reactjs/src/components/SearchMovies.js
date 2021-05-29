c90408b69086640e8302efa09f51058b9b0bfb58
nilesh1168 <n.suryawanshi1168@gmail.com>
2021-05-29 15:57:45 +0530

    added reset UI after operation

--- flixed_reactjs/src/components/SearchMovies.js
+++ flixed_reactjs/src/components/SearchMovies.js
@@ -4,19 +4,29 @@ import Form from 'react-bootstrap/Form'
 import Container from "react-bootstrap/Container";
 import Button from 'react-bootstrap/Button'
 import { Col } from "react-bootstrap";
-import {useState} from 'react';
+import { useState } from 'react';
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
 
 function SearchMovies(props) {
-    const [year,setYear] = useState(false);
+    const [year, setYear] = useState(false);
+    const [date,setDate] = useState(false);
+
+    const reset = ()=>{
+        if(year)
+            setYear(false)
+        if(date)
+            setDate(false)    
+        document.getElementById('movie').value = ""
+    }
+
 
     const addToWatchList = () => {
         let options = {
             method: 'POST',
             body: JSON.stringify(props.searchedMovie),
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json;charset=utf-8',
                 Authorization: `JWT ${localStorage.getItem('token')}`
             },
@@ -27,6 +37,7 @@ function SearchMovies(props) {
                 if (response.status === 201) {
                     console.log(props.searchedMovie.id)
                     props.handleWatchListAdd({ "title": props.searchedMovie.title, "id": props.searchedMovie.id })
+                    reset()
                 }
             })
             .catch(error => {
@@ -36,10 +47,14 @@ function SearchMovies(props) {
     }
 
     const addToWatchedList = () => {
+        var date = document.getElementById('watched_date')
+        if(date != null){
+            props.searchedMovie['watched_date'] = date.value
+        }
         let options = {
             method: 'POST',
             body: JSON.stringify(props.searchedMovie),
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json;charset=utf-8',
                 Authorization: `JWT ${localStorage.getItem('token')}`
             },
@@ -49,6 +64,7 @@ function SearchMovies(props) {
             .then(response => {
                 if (response.status === 201) {
                     props.handleWatchedListAdd({ "title": props.searchedMovie.title, "id": props.searchedMovie.id })
+                    reset()
                 }
             })
             .catch(error => {
@@ -57,7 +73,10 @@ function SearchMovies(props) {
     }
 
     const searchMovie = () => {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${encodeURIComponent(document.getElementById('movie_name').value)}&y=$`)
+        var year = document.getElementById('movie_year')
+        if(year === null)
+            year=""    
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${encodeURIComponent(document.getElementById('movie_name').value)}&y=${year.value}`)
             .then(response => {
                 if (response.ok) {
                     return response.json()
@@ -86,28 +105,41 @@ function SearchMovies(props) {
         <Row>
             <Container>
                 <Row className='my-3'>
-                        <Col className="col-md-4 text-center">
-                            <FormGroup>
-                                <Form.Control id="movie_name" type="text" placeholder="Enter Movie Name" />
-                            </FormGroup>
-                            <Button variant="primary" onClick={() => searchMovie()} type="button">Search</Button>
-                            <Button className="ml-3" disabled>Advanced Search</Button>
-                        </Col>
-                        <Col className="col-md-8">
-                            <Container className="text-center">
-                                <Form.Group>
-                                    <Form.Control id='movie' as="textarea" readOnly value={props.searchedMovie.title === undefined ? "" : `Title: ${props.searchedMovie.title}\nGenre: ${props.searchedMovie.genre}\nRating: ${props.searchedMovie.rating}\nYear: ${props.searchedMovie.year}\nRuntime: ${props.searchedMovie.runtime} min\nLanguage: ${props.searchedMovie.language}`} rows={6} />
-                                </Form.Group>
-                                <Row>
-                                    <Container>
-                                        <p style={{ color: "red" }}>{props.error}</p>
-                                    </Container>
-                                </Row>
-                                <Button id="add2watched" variant="primary" onClick={() => addToWatchedList()} type="button">Add to Watched</Button>
-                                <Button id="add2watchlist" className='ml-3' onClick={() => addToWatchList()} variant="primary" type="button" >Add to Watch List</Button>
-                                <Button disabled className="ml-3">Advanced Add</Button>
-                            </Container>
-                        </Col>
+                    <Col className="col-md-4 text-center">
+                        <FormGroup>
+                            <Form.Control id="movie_name" type="text" placeholder="Enter Movie Name" />
+                            {
+                            year ?
+                                <Form.Control className="my-3" id="movie_year" type="text" placeholder="Enter Movie Year" />
+                                : console.log("false")
+                        }
+                        </FormGroup>
+
+                        <Button variant="primary" onClick={() => searchMovie()} type="button">Search</Button>
+                        <Button className="ml-3" onClick={() => { setYear(!(year)) }}>Advanced Search</Button>
+                    </Col>
+                    <Col className="col-md-8">
+                        <Container className="text-center">
+                            <Form.Group>
+                                <Form.Control id='movie' as="textarea" readOnly value={props.searchedMovie.title === undefined ? "" : `Title: ${props.searchedMovie.title}\nGenre: ${props.searchedMovie.genre}\nRating: ${props.searchedMovie.rating}\nYear: ${props.searchedMovie.year}\nRuntime: ${props.searchedMovie.runtime} min\nLanguage: ${props.searchedMovie.language}`} rows={6} />
+                                {
+                                    date ? <><Form.Control className="my-3" id="watched_date" 
+                                                type="date" placeholder="Enter Date when watched" />    
+                                            <p style={{ color: "orange" }}>NOTE: Leave blank if watched today</p></>
+                                           : console.log("false") 
+                                }
+                            </Form.Group>
+
+                            <Row>
+                                <Container>
+                                    <p style={{ color: "red" }}>{props.error}</p>
+                                </Container>
+                            </Row>
+                            <Button id="add2watched" variant="primary" onClick={() => addToWatchedList()} type="button">Add to Watched</Button>
+                            <Button onClick={() => { setDate(!(date)) }} className="ml-3">Add Watched Date</Button>
+                            <Button id="add2watchlist" className='ml-3' onClick={() => addToWatchList()} variant="primary" type="button" >Add to Watch List</Button>
+                        </Container>
+                    </Col>
                 </Row>
                 <hr />
             </Container>
