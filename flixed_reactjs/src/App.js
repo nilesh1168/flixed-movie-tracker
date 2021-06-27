4997efa38862a8e63705c8a317cddc51cd43daeb
nilesh1168 <n.suryawanshi1168@gmail.com>
2021-06-27 21:34:02 +0530

    adde global lists states

--- flixed_reactjs/src/App.js
+++ flixed_reactjs/src/App.js
@@ -5,6 +5,8 @@ import RegisterForm from './components/RegisterForm'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import { isEqual } from "lodash";
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,12 +18,17 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      watchedList: [],
+      watchList: [],
       logged_in: localStorage.getItem('token') ? true : false,
       user: ''
     }
     this.handle_login = this.handle_login.bind(this)
     this.handle_logout = this.handle_logout.bind(this)
     this.handle_signup = this.handle_signup.bind(this)
+    this.handleWatchListAdd = this.handleWatchListAdd.bind(this);
+    this.handleWatchedListAdd = this.handleWatchedListAdd.bind(this);
+    this.handleWatchListDelete = this.handleWatchListDelete.bind(this);
   }
 
 
@@ -34,11 +41,11 @@ class App extends React.Component {
       })
         .then(res => res.json())
         .then(json => {
-          if(json.detail != null){
-            this.setState({ logged_in : false })
+          if (json.detail != null) {
+            this.setState({ logged_in: false })
           }
-          else{
-            console.log("alreadyt logged in "+JSON.stringify(json))
+          else {
+            console.log("alreadyt logged in " + JSON.stringify(json))
             this.setState({ user: json.username });
           }
         });
@@ -46,6 +53,56 @@ class App extends React.Component {
     else {
       console.log('User needs to log in')
     }
+    const watchedRequest = new Request('http://127.0.0.1:8000/movies/watched', {
+      method: 'GET',
+      headers: { Authorization: `JWT ${localStorage.getItem('token')}` }
+    })
+    const watchListRequest = new Request('http://127.0.0.1:8000/movies/watch_list', {
+      method: 'GET',
+      headers: { Authorization: `JWT ${localStorage.getItem('token')}` }
+    })
+    fetch(watchedRequest)
+      .then(response => {
+        if (response.ok)
+          return response.json()
+        else
+          throw new Error("Something went wrong!!!")
+      })
+      .then(data => {
+        var movies = []
+        data.map(movie => (
+          movies.push({ "title": movie.title, "id": movie.id })
+        ))
+        this.setState({
+          watchedList: movies
+        })
+      })
+      .catch(error => {
+        this.setState({
+          error: error.message
+        })
+      })
+    fetch(watchListRequest)
+      .then(response => {
+        if (response.ok)
+          return response.json()
+        else
+          throw new Error("Something went wrong!!!")
+      })
+      .then(data => {
+        var movies = []
+        data.map(movie => (
+          movies.push({ "title": movie.title, "id": movie.id })
+        ))
+        this.setState({
+          watchList: movies
+        })
+      })
+      .catch(error => {
+        this.setState({
+          error: error.message
+        })
+      })
   }
 
   handle_login = (e, data) => {
@@ -92,32 +149,62 @@ class App extends React.Component {
     this.setState({ logged_in: false, user: '' });
   };
 
+  handleWatchListAdd =(watchMovie) =>{
+    this.setState(
+      {
+        watchList: [...this.state.watchList, watchMovie]
+      }
+    )
+  }
+
+  handleWatchListDelete = (watchMovieIds) => {
+    watchMovieIds.forEach(delmovie => {
+      this.state.watchList.map((movie, index) => (
+        isEqual(JSON.stringify(delmovie), JSON.stringify(movie)) ? this.state.watchList.splice(index, 1) : console.log("false")
+      ))
+    });
+    this.setState({
+      watchList: this.state.watchList
+    })
+  }
+
+  handleWatchedListAdd = (watchedMovie) => {
+    this.setState(
+      {
+        watchedList: this.state.watchedList.concat(watchedMovie)
+      }
+    )
+  }
+
   render() {
-    var main_style = styles.main + " " +styles.main_background
-    if(this.state.logged_in)
+    var main_style = styles.main + " " + styles.main_background
+    if (this.state.logged_in)
       main_style = styles.main
     return (
       <Router>
         <div className={styles.App}>
           <NavBar user={this.state.user} handle_login={this.state.handle_login} logged_in={this.state.logged_in} handle_logout={this.handle_logout} />
-          <div  id="main" className={main_style}>
+          <div id="main" className={main_style}>
             <Switch>
               <Route path='/home'>
-                <Home logged_in={this.state.logged_in} />
+                <Home handleWatchedListAdd={this.handleWatchedListAdd} handleWatchListAdd={this.handleWatchListAdd} logged_in={this.state.logged_in} />
+              </Route>
+              <Route path='/dashboard'>
+                <Dashboard handleWatchListDelete={this.handleWatchListDelete} handleWatchedListAdd={this.handleWatchedListAdd} watchedList={this.state.watchedList} watchList={this.state.watchList} />
               </Route>
               <Route path="/login">
                 <LoginForm handle_login={this.handle_login} />
               </Route>
               <Route path="/register">
-                <RegisterForm handle_signup={ this.handle_signup }/>
+                <RegisterForm handle_signup={this.handle_signup} />
               </Route>
               <Route path="/">
               </Route>
             </Switch>
-              {
-                this.state.logged_in ? <Redirect to="/home"/>  : 
-                <Redirect to="/login"/>
-              }
+            {
+              this.state.logged_in ? <Redirect to="/home" /> :
+                <Redirect to="/login" />
+            }
           </div>
           <Footer />
         </div>
