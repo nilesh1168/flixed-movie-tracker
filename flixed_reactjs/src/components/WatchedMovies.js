feb60a63040cfd5750db7811a2a2124684d69762
nilesh1168 <n.suryawanshi1168@gmail.com>
2021-06-27 21:37:09 +0530

    added ListMovies component

--- flixed_reactjs/src/components/WatchedMovies.js
+++ flixed_reactjs/src/components/WatchedMovies.js
@@ -1,14 +1,10 @@
+import ListMovies from './ListMovies'
 
 function WatchedMovies(props) {
     return (
         <div className="flex flex-col justify-center items-center">
-            <h4 className="font-bold">This week's Watched Movies</h4>
             <div>
-                {
-                    props.watchedList.length === 0 ? <div>Empty</div> : props.watchedList.map(movie => (
-                        <div className="list-disc" key={movie.id} >{movie.title}</div>
-                    ))
-                }
+                <ListMovies type="list-disc" movieList={props.watchedList}/>
             </div>
         </div>
     )
