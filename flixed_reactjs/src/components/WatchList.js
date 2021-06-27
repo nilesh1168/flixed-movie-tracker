0bfa349c5402c1ec7169db27e972086ede0a7868
nilesh1168 <n.suryawanshi1168@gmail.com>
2021-06-27 21:37:20 +0530

    UI changes

--- flixed_reactjs/src/components/WatchList.js
+++ flixed_reactjs/src/components/WatchList.js
@@ -1,4 +1,4 @@
-import Movie from './Movie'
+import ListMovies from './ListMovies'
 
 function WatchList(props) {
 
@@ -6,7 +6,7 @@ function WatchList(props) {
         var selected_options = [...document.getElementById('selected_movies').selectedOptions]
         var ids = []
         selected_options.forEach(option => {
-            ids.push({"title":option.innerHTML,"id":option.id})
+            ids.push({ "title": option.innerHTML, "id": option.id })
         })
         return ids
     }
@@ -17,14 +17,14 @@ function WatchList(props) {
         var options = {
             method: 'PATCH',
             body: JSON.stringify({ "ids": ids }),
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json;charset=utf-8',
                 Authorization: `JWT ${localStorage.getItem('token')}`
             }
         }
         var moveRequest = new Request('http://127.0.0.1:8000/movies/watch_list', options)
         fetch(moveRequest).then(response => {
-            if (response){
+            if (response) {
                 console.log(response)
                 props.handleWatchListDelete(ids)
                 props.handleWatchedListAdd(ids)
@@ -42,7 +42,7 @@ function WatchList(props) {
         var options = {
             method: 'DELETE',
             body: JSON.stringify({ "ids": ids }),
-            headers: { 
+            headers: {
                 'Content-Type': 'application/json;charset=utf-8',
                 Authorization: `JWT ${localStorage.getItem('token')}`
             }
@@ -64,20 +64,20 @@ function WatchList(props) {
 
     return (
         <div className="flex flex-col justify-center items-center">
-            <h4 className="font-bold">Top 5 Movies to Watch </h4>
             <div>
-                <ol className="list-decimal" id="selected_movies" multiple>
                     {
-                        props.watchList.length === 0 ? <Movie movieName="Empty"></Movie> : props.watchList.map(movie => (
-                            <Movie key={movie.id} id={movie.id} movieName={movie.title}></Movie>
-                        ))
+                        props.watchList.length === 0 ? <div>Empty</div> :
+                <select className="text-center p-3 border outline-none border-gray-300 focus:ring-2 focus:ring-yellow-400 focus:border-yellow-600" id="selected_movies" multiple>
+                            {props.watchList.map(movie => (
+                                <option key={movie.id} id={movie.id} movieName={movie.title}>{movie.title}</option>
+                            ))}
+                </select>
                     }
-                </ol>
             </div>
-            {/* <button className="md:mt-0 mt-3 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
+            <button className="mt-3 text-white bg-blue-500 border-0 py-2 px-5 focus:outline-none hover:bg-blue-600 rounded text-lg"
                 onClick={() => moveToWatched()}>Move to Watched</button>
-            <button className="md:mt-0 my-3 text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg"
-                onClick={() => removeFromWatch()} >Remove</button> */}
+            <button className="my-3 text-white bg-blue-500 border-0 py-2 px-5 focus:outline-none hover:bg-blue-600 rounded text-lg"
+                onClick={() => removeFromWatch()} >Remove</button>
         </div>
     )
 }
