ce07ab846bce7aab2162e6686932edd12cd1fba1
nilesh1168 <n.suryawanshi1168@gmail.com>
2023-12-22 15:29:43 -0500

    updated models to support TMDB api data

--- flixed_django/flixedREST/models.py
+++ flixed_django/flixedREST/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class WatchedMovie(models.Model):
     id = models.CharField(max_length=50,primary_key=True)
+    imdb_id = models.CharField(max_length=50)
     title = models.CharField(max_length=300)
     rating = models.DecimalField(max_digits=2,decimal_places=1)
     genre = models.CharField(max_length=300)
@@ -13,6 +14,7 @@ class WatchedMovie(models.Model):
     watched_date = models.DateField(auto_now_add = True)
     times_watched = models.IntegerField(default=1)
     language = models.CharField(max_length=100)
+    imageUrl = models.CharField(max_length=100)
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="watched_movies")
     
     class Meta:
@@ -31,18 +33,20 @@ class WatchedMovie(models.Model):
         
 class WatchList(models.Model):
     id = models.CharField(max_length=50,primary_key=True,null=False)
+    imdb_id = models.CharField(max_length=50)
     title = models.CharField(max_length=300,null=False)
     rating = models.DecimalField(max_digits=2,decimal_places=1,null=False)
     genre = models.CharField(max_length=300,null=False)
     year = models.IntegerField()
     runtime = models.IntegerField(null=False)
     language = models.CharField(max_length=100)
+    imageUrl = models.CharField(max_length=100)
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="watch_list",null=False)
 
 
     class Meta:
-        verbose_name = "Watch List"
-        verbose_name_plural = "Watch List"
+        verbose_name = "WatchList"
+        verbose_name_plural = "WatchList"
 
     def __str__(self):
         return self.title
