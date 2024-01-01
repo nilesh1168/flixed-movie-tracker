ea25daf354ae0f8633b0500615fdf790be645d1a
nilesh1168 <n.suryawanshi1168@gmail.com>
2024-01-01 11:14:19 -0500

    changed variable name

--- flixed_django/flixedREST/serializers.py
+++ flixed_django/flixedREST/serializers.py
@@ -19,8 +19,7 @@ class UserSerializerWithToken(serializers.ModelSerializer):
     token = serializers.SerializerMethodField()
     password = serializers.CharField(write_only=True)
 
-    def get_token(self, obj): 
-        print("inside get_token UserSerializerWithToken")
+    def get_token(self, user): 
         refresh = RefreshToken.for_user(user)
         return {
             'refresh': str(refresh),
