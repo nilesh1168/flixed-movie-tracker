c3973ad7e6ed4fdb5b36147ef478f5b0662d0be1
nilesh1168 <n.suryawanshi1168@gmail.com>
2024-04-05 19:15:38 -0400

    added checkbox to initiate TV and movie filter development

--- flixed_reactjs/src/components/SearchMovies.js
+++ flixed_reactjs/src/components/SearchMovies.js
@@ -31,7 +31,7 @@ function SearchMovies(props) {
                 Authorization: `Bearer ${localStorage.getItem('token')}`
             },
         }
-        let request = new Request(`${base_url}/search?query=${encodeURIComponent(document.getElementById('movie_name').value)}&language=${default_lang}&page=${currentPage}`, options)
+        let request = new Request(`${base_url}/search/movie?query=${encodeURIComponent(document.getElementById('movie_name').value)}&language=${default_lang}&page=${currentPage}`, options)
         // console.log(`${props.configs.BASE_URL}/search?query=${encodeURIComponent(document.getElementById('movie_name').value)}&language=${props.configs.DEFAULT_LANG}&page=${props.configs.DEFAULT_PAGE_NUMBER}`)
         console.log(request.url)
         fetch(request, options)
@@ -51,7 +51,7 @@ function SearchMovies(props) {
                 data.results.forEach(element => {
                     var movieItem = {}
                     movieItem.id = element.id
-                    movieItem.media_type = element.media_type
+                    movieItem.media_type = element.media_type = 'movie'     // update this line when adding support for TV and Movie both
                     if (element.media_type === "movie") {
                         movieItem.title = element.title
                         movieItem.release_date = element.release_date
@@ -101,55 +101,55 @@ function SearchMovies(props) {
         var data = {
             "id": movieId
         }
-            let options = {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8',
-                    Authorization: `Bearer ${localStorage.getItem('token')}`
-                },
-            }
-            let request = new Request(`${base_url}/movies/watch_list`, options)
-            fetch(request)
-                .then(response => {
-                    if (response.status === 201) {
-                        console.log("added movie")
-                        props.handleError("added movie to watchlist")
-                        // console.log(props.searchedMovie.id)
-                        // props.handleWatchListAdd({ "title": props.searchedMovie.title, "id": props.searchedMovie.id })
-                    }
-                })
-                .catch(error => {
-                    console.log(error.message)
-                    props.handleError(error.message)
-                })
+        let options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+        }
+        let request = new Request(`${base_url}/movies/watch_list`, options)
+        fetch(request)
+            .then(response => {
+                if (response.status === 201) {
+                    console.log("added movie")
+                    props.handleError("added movie to watchlist")
+                    // console.log(props.searchedMovie.id)
+                    // props.handleWatchListAdd({ "title": props.searchedMovie.title, "id": props.searchedMovie.id })
+                }
+            })
+            .catch(error => {
+                console.log(error.message)
+                props.handleError(error.message)
+            })
     }
 
     const addToWatchedList = (movieId) => {
-            var data = {
-                "id": movieId
-            }
-            let options = {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8',
-                    Authorization: `Bearer ${localStorage.getItem('token')}`
-                },
-            }
-            let request = new Request(`${base_url}/movies/watched`, options)
-            fetch(request)
-                .then(response => {
-                    if (response.status === 201) {
-                        console.log("added movie")
-                        props.handleError("Movie added successfully to watched")
-                        // show a alert that movie has been added successfully
-                        // props.handleWatchedListAdd({ "title": props.searchedMovie.title, "id": props.searchedMovie.id })
-                    }
-                })
-                .catch(error => {
-                    props.handleError(error.message)
-                })
+        var data = {
+            "id": movieId
+        }
+        let options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+        }
+        let request = new Request(`${base_url}/movies/watched`, options)
+        fetch(request)
+            .then(response => {
+                if (response.status === 201) {
+                    console.log("added movie")
+                    props.handleError("Movie added successfully to watched")
+                    // show a alert that movie has been added successfully
+                    // props.handleWatchedListAdd({ "title": props.searchedMovie.title, "id": props.searchedMovie.id })
+                }
+            })
+            .catch(error => {
+                props.handleError(error.message)
+            })
     }
 
 
@@ -168,17 +168,35 @@ function SearchMovies(props) {
     return (
         <div className="row">
             <div className="container">
-                <div className='row my-3 g-2'>
+                <div className='row my-3'>
                     <div className="col-md-4">
                     </div>
                     <div className="col-md-4 text-center">
-                        <input className="form-control" id="movie_name" type="text" placeholder="Enter Movie Name" />
-                        <button className="btn btn-outline-dark mt-2" onClick={() => searchMovie()} type="button">Search</button>
-                        <button className="btn btn-outline-dark mt-2 mx-3" disabled>Advanced Search</button>
+                        <div className="container">
+                            <input className="form-control" id="movie_name" type="text" placeholder="Enter Name" />
+                            <button className="btn btn-outline-dark mt-2" onClick={() => searchMovie()} type="button">Search </button>
+                            <button className="btn btn-outline-dark mt-2 mx-3" disabled>Advanced Search</button>
+                        {/* </div>
+                        <div className="flex-row"> */}
+                        </div>
                     </div>
                     <div className="col-md-4">
+                    <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="movieCheckChecked" checked/>
+                                <label class="form-check-label" for="movieCheckChecked">
+                                    Movie
+                                </label>
+                            </div>
+                            {/* <div class="form-check">
+                                <input class="form-check-input" type="checkbox" id="tvCheckChecked" />
+                                <label class="form-check-label" for="tvCheckChecked">
+                                    TV
+                                </label>
+                            </div> */}
                     </div>
                 </div>
+                <div className="text-center"><p> <span style={{color:'red'}}>*</span> Current media type support for movies only.</p></div>
+                <hr className="my-2"></hr>
                 <div className="row">
                     <div className="container text-center">
                         <p style={{ color: "red" }}>{props.error}</p>
@@ -191,17 +209,16 @@ function SearchMovies(props) {
                                 <div className="accordion" id="movieAccordion">
                                     {
                                         searchBtnClicked ?
-                                        movieMap.map(element => (
-                                            element.media_type === "movie" ?
-                                            <MovieItem key={element.id} id={element.id} type={element.media_type} poster_path={element.poster_path} title={element.title} release_date={element.release_date} addToWatchList={addToWatchList} addToWatchedList={addToWatchedList} secure_base_url={secure_base_url} image_size={image_size}/>
-                                            :
-                                            <MovieItem key={element.id} id={element.id} type={element.media_type} poster_path={element.poster_path} name={element.name} first_air_date={element.first_air_date} addToWatchList={addToWatchList} addToWatchedList={addToWatchedList} secure_base_url={secure_base_url} image_size={image_size}/>
+                                            movieMap.map(element => (
+                                                element.media_type === "movie" ?
+                                                    <MovieItem key={element.id} id={element.id} type={element.media_type} poster_path={element.poster_path} title={element.title} release_date={element.release_date} addToWatchList={addToWatchList} addToWatchedList={addToWatchedList} secure_base_url={secure_base_url} image_size={image_size} />
+                                                    :
+                                                    <MovieItem key={element.id} id={element.id} type={element.media_type} poster_path={element.poster_path} name={element.name} first_air_date={element.first_air_date} addToWatchList={addToWatchList} addToWatchedList={addToWatchedList} secure_base_url={secure_base_url} image_size={image_size} />
                                             ))
                                             :
                                             <div className="text-center"><p>Perform Search</p></div>
-                                        }
+                                    }
                                 </div>
-                                <div className="text-center"><p>* Current media type support for movies only.</p></div>
                             </div>
                         </div>
                         <div className="row">
@@ -210,7 +227,7 @@ function SearchMovies(props) {
                                     searchBtnClicked ?
                                         <div>
                                             <button id="prevBtn" disabled={currentPage === 1 ? true : false} className="btn btn-outline-dark mt-2" onClick={() => decrementPage()} type="button">Prev</button>
-                                            <button className="btn mt-2" style={{cursor: "default"}}>{currentPage} of {totalPages.current}</button>
+                                            <button className="btn mt-2" style={{ cursor: "default" }}>{currentPage} of {totalPages.current}</button>
                                             <button id="nextBtn" disabled={currentPage === totalPages.current ? true : false} className="btn btn-outline-dark mt-2" onClick={() => incrementPage()} type="button">Next</button>
                                         </div>
                                         : <div></div>
