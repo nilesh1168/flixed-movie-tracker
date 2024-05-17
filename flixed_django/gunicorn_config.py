a2093586da696221d1452998d5780ec8748769e8
nilesh1168 <n.suryawanshi1168@gmail.com>
2024-05-17 17:25:32 -0400

    gunicorn config

--- flixed_django/gunicorn_config.py
+++ flixed_django/gunicorn_config.py
@@ -1,7 +1,16 @@
 import multiprocessing
+import os
 
 bind = "127.0.0.1:8000"  # Replace with your desired IP and port
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gthread"
 threads = 2
 timeout = 60
+# Access log - records incoming HTTP requests
+accesslog = "/Users/nilesh/Work/flixed-movie-tracker/logs/gunicorn.access.log"
+# Error log - records Gunicorn server goings-on
+errorlog = "/Users/nilesh/Work/flixed-movie-tracker/logs/gunicorn.error.log"
+# Whether to send Django output to the error log 
+capture_output = True
+# How verbose the Gunicorn error logs should be 
+loglevel = "info"
