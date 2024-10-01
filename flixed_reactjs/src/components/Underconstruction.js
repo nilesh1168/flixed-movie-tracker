7e6fe0ebcea57103500fe690cf8e99e6323c05b3
nilesh1168 <n.suryawanshi1168@gmail.com>
2024-10-01 16:31:48 -0400

    underconst

--- flixed_reactjs/src/components/Underconstruction.js
+++ flixed_reactjs/src/components/Underconstruction.js
@@ -1,23 +1,28 @@
-import workingImage from '../styles/images/working-new.png'
+import { Component } from "react"
+import image from '../styles/images/Inprogress.jpg'
 import styles from '../styles/App.module.css'
-function UnderProduction() {
-    return (
-        <>
-            <div class="grid-cols-3 gap-4 md:grid">
-                <div></div>
-                <div>
-                    <img alt="under-construction" className={styles.working} src={workingImage}></img>
+class UnderConstruction extends Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row align-items-center">
+                    <div className="col-md-6">
+                        <h1>
+                            <p>We are working on it.</p>
+                        </h1>
+                    </div>
+                    <div className="col-md-6">                        
+                        <img src={image} className="img-fluid"></img>
+                    </div>
                 </div>
-                <div></div>
             </div>
-            <div class="flex flex-col justify-center">
-                <p className="text-xl text-center">Sit back and Relax!</p>
-                <div class="justify-center">
-                    <p className="text-xl text-center">We are working on something <span className="text-2xl">AWESOME!</span></p>
-                </div>
-            </div>
-        </>
-    )
+        )
+    }
+
 }
 
-export default UnderProduction
+export default UnderConstruction
