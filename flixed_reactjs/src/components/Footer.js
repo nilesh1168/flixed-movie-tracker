08618238939b8743a48ee4511b9cf3a256a44c85
nilesh1168 <n.suryawanshi1168@gmail.com>
2024-12-18 15:22:03 -0500

    added landing page and app name

--- flixed_reactjs/src/components/Footer.js
+++ flixed_reactjs/src/components/Footer.js
@@ -1,4 +1,5 @@
 import cssStyle from "../styles/App.module.css";
+import tmdb_logo from "../styles/images/TMDB-ico.svg"
 function Footer() {
 
     const styles = {
@@ -12,8 +13,10 @@ function Footer() {
         <footer id="footer" className={cssStyle.flixed_footer}>
             <h3 className={cssStyle.flixed_name}>Flixed</h3>
             <div style={styles.verticalLine}></div>
+            <img src={tmdb_logo} alt="TMDB" width="60"></img>
+            <div style={styles.verticalLine}></div>
             <div style={{textAlign:'center'}}>
-                <p>&copy; Copyright 2023 </p>
+                <p>&copy; Copyright 2024 </p>
                 <p>Designed & Developed by <a style={{color: '#E98074'}} href="http://nilesh1168.github.io">Nilesh Suryawanshi</a></p>
             </div>
 
--- flixed_reactjs/src/index.js
+++ flixed_reactjs/src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 ReactDOM.render(
   <App/>,
