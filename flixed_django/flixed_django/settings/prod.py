39060ddb1fbb69936fcc0335aefa1e10df6bf368
nilesh1168 <n.suryawanshi1168@gmail.com>
2024-12-18 03:02:40 -0500

    dev and prod settings for backend

---
+++ flixed_django/flixed_django/settings/prod.py
@@ -0,0 +1,15 @@
+from .commons import *
+import dj_database_url, os
+
+DATABASES = {
+    'default': dj_database_url.parse(os.getenv('RENDER_DB'))
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = [os.getenv('FLIXED_REACT_URL')] # REACT_HOST
+
+CORS_ORIGIN_WHITELIST = (
+       os.getenv("CORS_ORIGIN_WHITELIST"),
+)
