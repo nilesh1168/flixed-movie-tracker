4207a75711185ad700e7db5ae205937e00c58f7d
nilesh1168 <n.suryawanshi1168@gmail.com>
2025-01-02 19:09:36 -0500

    moved files

--- flixed_reactjs/src/components/constants.js
+++
@@ -1,9 +0,0 @@
-class Constants{
-    static MOVIE = "movie";
-    static TV = "tv";
-    static WATCHED = "watched";
-    static NUMBER_OF_REC_PER_PAGE = 15;
-    static DEF_IMG_SIZE = "original";
-}
-
-export default Constants
